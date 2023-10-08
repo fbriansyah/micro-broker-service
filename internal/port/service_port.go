4ab1b88a9c680a3a25b366bce97ea76f41c7f9e9
Febriansyah <fbriansyah@gmail.com>
2023-10-08 12:36:58 +0700

    add comment

--- internal/port/service_port.go
+++ internal/port/service_port.go
@@ -9,8 +9,14 @@ import (
 )
 
 type BrokerServicePort interface {
+	// Login send login request to auth microservice
 	Login(ctx context.Context, username, password string) (dmsession.Session, error)
+	// Register send register request to auth microservice
 	Register(ctx context.Context, user dmuser.User, password string) (dmuser.User, error)
+	// Inquiry check token validity to session microservice.
+	// If valid, send inquiry request to payment microservice.
 	Inquiry(ctx context.Context, billNumber, productCode, token string) (dmbiller.Bill, error)
+	// Payment check token validity to session microservice.
+	// If valid, send Payment request to payment microservice.
 	Payment(ctx context.Context, amount int64, inqID, token string) (dmbiller.Transaction, error)
 }
