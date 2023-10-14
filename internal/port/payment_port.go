5650dc17f099d6b06481a809becaea004266c380
Febriansyah <fbriansyah@gmail.com>
2023-10-14 18:05:31 +0700

    add list product and get balance signature to port

--- internal/port/payment_port.go
+++ internal/port/payment_port.go
@@ -11,4 +11,6 @@ import (
 type PaymentPort interface {
 	Inquiry(ctx context.Context, in *payment.InquiryRequest, opts ...grpc.CallOption) (*payment.InquiryResponse, error)
 	Payment(ctx context.Context, in *payment.PaymentRequest, opts ...grpc.CallOption) (*payment.PaymentResponse, error)
+	ListProduct(ctx context.Context, in *payment.ListProductRequest, opts ...grpc.CallOption) (payment.PaymentService_ListProductClient, error)
+	GetBalance(ctx context.Context, in *payment.GetBalanceRequest, opts ...grpc.CallOption) (*payment.GetBalanceResponse, error)
 }
